738cc65364063b80ac2f9c7dcd663ae274028277
Chris O'Sullivan <thechrisoshow@gmail.com>
2016-09-23 12:01:52 +0100

    Better error handling

--- lib/outreach/errors.rb
+++ lib/outreach/errors.rb
@@ -5,16 +5,29 @@ module Outreach
     class Unauthorized < StandardError; end
 
     def check_for_error(status_code, response_body)
+      response_hash = JSON.parse(response_body)
+      if response_hash['errors'].present? &&
+          response_hash['errors']['status'].present?
+        status_code = response_hash['errors']['status']
+      end
+
       # raise error if status code isn't 200
       case status_code.to_i
       when 401
-        description = begin
-          JSON.parse(response_body).fetch("error_description")
-        rescue
-          "Unauthorized"
-        end
-        raise Unauthorized.new(description)
+        unauthorized_error(response_body)
       end
     end
+
+    private
+
+    def unauthorized_error(response_body)
+      description = begin
+        JSON.parse(response_body).fetch("error_description")
+      rescue
+        "Unauthorized"
+      end
+      raise Unauthorized.new(description)
+    end
+
   end
 end
